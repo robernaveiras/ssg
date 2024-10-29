0dfb472e91af8ff5ce317dc39089edb22b5d1b8a
robernaveiras <mephysto@me.com>
2024-10-29 19:46:47 +0100

    Initial commint

---
+++ public/src/textnode.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type.value
+        self.url = url
+
+class __eq
