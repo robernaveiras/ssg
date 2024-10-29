db467453d1df65dc6c0102ff3261a91c4728acd8
robernaveiras <mephysto@me.com>
2024-10-29 21:55:46 +0100

    Some changes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    # Create a test TextNode here
+    # For example, you could create a bold text node with a URL
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from typing import Self
+
 
 class TextType(Enum):
     NORMAL = "normal"
@@ -8,10 +10,19 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type.value
         self.url = url
 
-class __eq
+    def __eq__(self, other):
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
